1fbd8c98b1b28651e8bf8cfcaf35980741155156
Daniel Harazim <73634270+DanielHarazim@users.noreply.github.com>
2020-11-05 09:39:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,37 +6,33 @@
 <body>
     <h1>imie</h1>
 <?php
-//     $servername = "sql7.freemysqlhosting.net";
-//     $username = "sql7374585";
-//     $password = "dDfwp2YyVS";
-//     $dbname = "sql7374585";
+     $servername = "sql7.freemysqlhosting.net";
+    $username = "sql7374585";
+    $password = "dDfwp2YyVS";
+    $dbname = "sql7374585";
 
-//     $conn = new mysqli("$servername","$username","password","dbname");
-//     if ($conn->connect_error) {
-//         die("Connection failed: " . $conn->connect_error);
-//     }
-//     echo "Connected successfully";
+    $conn = new mysqli("$servername","$username","password","dbname");
+    if ($conn->connect_error) {
+       die("Connection failed: " . $conn->connect_error);
+     }
+     echo "Connected successfully";
 
-//     $sql = "SELECT * FROM pracownicy";
-    
-//     if ($conn->query($sql) === TRUE) {
+    $sql = "SELECT * FROM pracownicy";
+        if ($conn->query($sql) === TRUE) {
             
-//             echo "ok";
-//     } else {
-//         echo "Error: " . $sql . "<br>" . $conn->error;
-//     }
-    
-//     $wynik = mysqli_query($conn, $sql);
+             echo "ok";     } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+   $wynik = mysqli_query($conn, $sql);
 
-//     echo('<table border="1">');
-//     echo('<th>Imie</th><th>zarobki</th>');
+     echo('<table border="1">');
+     echo('<th>Imie</th><th>zarobki</th>');
 
-//     while($wiersz=mysqli_fetch_assoc($wynik)){
-//         echo('<tr>');
-//         echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
-//         echo('</tr>');
-//     }
-//       echo('</table>');
+    while($wiersz=mysqli_fetch_assoc($wynik)){
+       echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('</tr>');    }
+      echo('</table>');
 
 ?>
 </body>
