ab313fa69546e271c5eba82a75d041bd6bac51bd
Tomjand <domagala.tomasz@gmail.com>
2020-11-05 09:32:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,15 +9,23 @@
     $username = "sql7374585";
     $password = "dDfwp2YyVS";
     $dbname = "sql7374585";
-      echo('</table>');
-    if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-$conn = new mysqli("$servername","$username","password","dbname");
-$sql = "SELECT * FROM pracownicy";
-$wynik = mysqli_query($conn, $sql);
 
+    $conn = new mysqli("$servername","$username","password","dbname");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+
+    $sql = "SELECT * FROM pracownicy";
+    
+    if ($conn->query($sql) === TRUE) {
+            
+            echo "ok";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
+    $wynik = mysqli_query($conn, $sql);
 
     echo('<table border="1">');
     echo('<th>Imie</th><th>zarobki</th>');
@@ -27,7 +35,7 @@ $wynik = mysqli_query($conn, $sql);
         echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
         echo('</tr>');
     }
-
+      echo('</table>');
 
 ?>
 </body>
