7ceb8a8db933b3ba6c3fe7a180e0a7aa81875696
Daniel Harazim <73634270+DanielHarazim@users.noreply.github.com>
2020-11-05 09:23:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
     $username = "217198";
     $password = "Danielharazim123";
     $dbname = "217198";
+      echo('</table>');
+    if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 $conn = new mysqli("$servername","$username","password","dbname");
 $sql = "SELECT * FROM pracownicy";
 $wynik = mysqli_query($conn, $sql);
@@ -23,11 +28,6 @@ $wynik = mysqli_query($conn, $sql);
         echo('</tr>');
     }
 
-    echo('</table>');
-    if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
 
 ?>
 </body>
