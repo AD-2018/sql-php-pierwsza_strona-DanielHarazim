098684864feb54a892121f5e6ec04229b49fe61e
Tomjand <domagala.tomasz@gmail.com>
2020-11-05 09:33:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,38 +4,39 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <h1>imie</h1>
 <?php
-    $servername = "sql7.freemysqlhosting.net";
-    $username = "sql7374585";
-    $password = "dDfwp2YyVS";
-    $dbname = "sql7374585";
+//     $servername = "sql7.freemysqlhosting.net";
+//     $username = "sql7374585";
+//     $password = "dDfwp2YyVS";
+//     $dbname = "sql7374585";
 
-    $conn = new mysqli("$servername","$username","password","dbname");
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully";
+//     $conn = new mysqli("$servername","$username","password","dbname");
+//     if ($conn->connect_error) {
+//         die("Connection failed: " . $conn->connect_error);
+//     }
+//     echo "Connected successfully";
 
-    $sql = "SELECT * FROM pracownicy";
+//     $sql = "SELECT * FROM pracownicy";
     
-    if ($conn->query($sql) === TRUE) {
+//     if ($conn->query($sql) === TRUE) {
             
-            echo "ok";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+//             echo "ok";
+//     } else {
+//         echo "Error: " . $sql . "<br>" . $conn->error;
+//     }
     
-    $wynik = mysqli_query($conn, $sql);
+//     $wynik = mysqli_query($conn, $sql);
 
-    echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
+//     echo('<table border="1">');
+//     echo('<th>Imie</th><th>zarobki</th>');
 
-    while($wiersz=mysqli_fetch_assoc($wynik)){
-        echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
-        echo('</tr>');
-    }
-      echo('</table>');
+//     while($wiersz=mysqli_fetch_assoc($wynik)){
+//         echo('<tr>');
+//         echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+//         echo('</tr>');
+//     }
+//       echo('</table>');
 
 ?>
 </body>
