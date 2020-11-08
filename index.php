a8e72a407e844ba22f3d829df28a2d811cefed80
Daniel Harazim <73634270+DanielHarazim@users.noreply.github.com>
2020-11-08 09:11:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,23 +17,20 @@
      }
      echo "Connected successfully";
 
-    $sql = "SELECT * FROM pracownicy";
+    $sql ="select * from pracownicy";
+    $result = mysqli_query($conn, $sql);
+    echo('<table border="1" class="tabela"');
+    echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
+    while($row=mysqli_fetch_assoc($result)){ echo("<tr>"); 
+                                            {echo("<tr>");
+      echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>")
+                                            echo("<tr>"); echo("</tr>");}
         if ($conn->query($sql) === TRUE) {
             
              echo "ok";     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
-   $wynik = mysqli_query($conn, $sql);
+                                            echo('</table>'); ?>
 
-     echo('<table border="1">');
-     echo('<th>Imie</th><th>zarobki</th>');
-
-    while($wiersz=mysqli_fetch_assoc($wynik)){
-       echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
-        echo('</tr>');    }
-      echo('</table>');
-
-?>
 </body>
 </html>
