5d42d36f65caf38f80fce61d70b82aa9109bf791
Daniel Harazim <73634270+DanielHarazim@users.noreply.github.com>
2020-11-08 09:01:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <h1>Tabeleczkaaaaa</h1>
 <?php
     $servername = "sql7.freemysqlhosting.net";
-    $username = "daniel.harazim123@gmail.com";
+    $username = "daniel";
     $password = "dDfwp2YyVS";
     $dbname = "sql7374585";
 
