5360e2c4efa5b83cf7a58303d0bbe3396098c885
Daniel Harazim <73634270+DanielHarazim@users.noreply.github.com>
2020-11-09 19:30:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $wynik = mysqli_query($conn, $sql);
     echo('<table border="1">');
     echo('<th>Imie</th><th>zarobki</th><th>dział</th>');
   
-    echo(<p><th>Imie</th><th>zarobki</th><th>dział</th></p>);
+    
     while($wiersz=mysqli_fetch_assoc($wynik)){
         echo('<tr>');
         echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>'.'<td>'.$wiersz['dział'].'</td>');
