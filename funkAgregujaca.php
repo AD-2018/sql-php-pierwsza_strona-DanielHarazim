7c25a9a510c70e0811086833d85bc6b234a14129
Daniel Harazim <73634270+DanielHarazim@users.noreply.github.com>
2020-11-26 08:53:06 +0100

    Update funkAgregujaca.php

--- funkAgregujaca.php
+++ funkAgregujaca.php
@@ -6,7 +6,7 @@
 <body>
 
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-DanielHarazim">Github</a>
-    <a href="index.php">Pracownicy</a>
+    <a href="index.php">Strona główna</a>
     <a href="pracownicyiorganizacja.php">Organizacja Pracownicy</a>
     <a href="funkAgregujaca.php">Funkcje Agregujace</a>
      <a href="formularz.html">Formularz</a>
