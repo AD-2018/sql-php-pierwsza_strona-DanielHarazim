22462d326f2a6a52fbc94e130805438debd27d96
Daniel Harazim <73634270+DanielHarazim@users.noreply.github.com>
2020-12-03 09:33:01 +0100

    Update danedobazy.php

--- danedobazy.php
+++ danedobazy.php
@@ -31,3 +31,21 @@ Usuwanie pracownika<br>
 <?php
 require "connect.php";
 
+$conn= new mysqli($servername,$username,$password,$dbname);  
+     
+$sql ="select * from pracownicy"; 
+echo("<h3>Tabela Pracowników</h3>"); 
+    echo("<li>".$sql);
+$result = mysqli_query($conn, $sql);  
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
+?>
+</body>
+</html>
