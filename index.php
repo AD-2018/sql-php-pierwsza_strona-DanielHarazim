7852ce18068e6cb24cc2f1a74672a8511e8ceda1
Daniel Harazim <73634270+DanielHarazim@users.noreply.github.com>
2021-02-04 13:41:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,11 +13,11 @@
     <h1>Daniel Harazim</h1>
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-DanielHarazim">github</a>
     <br>
-    <a href="pracownicy/pracownicyiorganizacja.php">Organizacja i Pracownicy</a>
-    <a href="pracownicy/funkAgregujaca.php">Funkcje agregujące</a>
-  <a href="pracownicy/DataiCzas.php">Data i Czas</a>
-  <a href="FORMULARZ/formularz.html">Formularz</a>
-    <a href="pracownicy/danedobazy.php">Dane do bazy</a>
+    <a href="/pracownicy/pracownicyiorganizacja.php">Organizacja i Pracownicy</a>
+    <a href="/pracownicy/funkAgregujaca.php">Funkcje agregujące</a>
+  <a href="/pracownicy/DataiCzas.php">Data i Czas</a>
+  <a href="/formularz/formularz.html">Formularz</a>
+    <a href="/pracownicy/danedobazy.php">Dane do bazy</a>
 </body>
 </html>
 <?php
