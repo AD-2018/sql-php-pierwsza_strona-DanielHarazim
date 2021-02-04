0b566945014a3c1a1ca988067157b534716251e1
Daniel Harazim <73634270+DanielHarazim@users.noreply.github.com>
2021-02-04 13:46:14 +0100

    Create index.php

---
+++ pracownicy/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+  
+    
+</head>
+<body>
+    <h1>Daniel Harazim</h1>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-DanielHarazim">github</a>
+    <br>
+    <a href="/pracownicy/pracownicyiorganizacja.php">Organizacja i Pracownicy</a>
+    <a href="/pracownicy/funkAgregujaca.php">Funkcje agregujące</a>
+  <a href="/pracownicy/DataiCzas.php">Data i Czas</a>
+  <a href="/formularz/formularz.html">Formularz</a>
+    <a href="/pracownicy/danedobazy.html">Dane do bazy</a>
+</body>
+</html>
+<?php
+require_once("connect.php");
+
+
+
+?>
+</body>
+</html>
