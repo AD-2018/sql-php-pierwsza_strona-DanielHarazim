78e95004405656d82ceed528d08891906af538bf
Daniel Harazim <73634270+DanielHarazim@users.noreply.github.com>
2021-02-04 13:19:37 +0100

    Update pracownicyiorganizacja.php

--- pracownicy/pracownicyiorganizacja.php
+++ pracownicy/pracownicyiorganizacja.php
@@ -8,13 +8,13 @@
 </head>
 <body>
     <h1>Daniel Harazim</h1>
-    <a href="index.php">strona główna</a>
+    <a href="pracownicy/index.php">strona główna</a>
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-DanielHarazim">github</a>
     <br>
-    <a href="funkAgregujaca.php">Funkcje agregujące</a>
-    <a href="DataiCzas.php">Data i Czas</a>
-     <a href="formularz.html">Formularz</a>
-    <a href="danedobazy.html">Dane do bazy</a>
+    <a href="pracownicy/funkAgregujaca.php">Funkcje agregujące</a>
+    <a href="pracownicy/DataiCzas.php">Data i Czas</a>
+     <a href="pracownicy/formularz.html">Formularz</a>
+    <a href="pracownicy/danedobazy.html">Dane do bazy</a>
 </body>
 </html>
 <?php
