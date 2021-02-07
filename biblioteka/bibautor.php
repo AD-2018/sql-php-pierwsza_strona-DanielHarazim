a7a8a1a4605baf3cd691bc7a903b69a6d1f2e08d
Daniel Harazim <73634270+DanielHarazim@users.noreply.github.com>
2021-02-07 10:10:11 +0100

    Update bibautor.php

--- biblioteka/bibautor.php
+++ biblioteka/bibautor.php
@@ -1,1 +1,18 @@
+<?php
+echo("bibautor.php");
+echo "<li>".$_POST['autor'];
 
+require_once("../connect.php");
+
+$sql = "INSERT INTO bibl_autor (id_autor, autor) 
+       VALUES (null, '".$_POST['autor']."')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+  header('Location:https://php-harazim.herokuapp.com/biblioteka/biblioteka.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
